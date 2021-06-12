8eb9c80581eb2c6b9823cb086bf72068793c423e
EdisonStalin[C <edison.jumbo@epn.edu.ec>
2021-06-12 16:43:54 -0500

    V_Final

--- catalogo/js/action.js
+++ catalogo/js/action.js
@@ -1,5 +1,29 @@
-window.addEventListener("scroll",function(){
-    var header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
-    console.log(window.scrollY)
+window.addEventListener('scroll',function(){
+    var header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
 })
+
+
+//window.addEventListener("scroll",function(){
+//    var element = document.querySelector(".container_up");
+//   element.classList.toggle("show_up", window.scrollY > 650);
+//})
+
+
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 650){
+        document.querySelector('.container_up')
+        .classList.add('show_up');
+    }else{
+        document.querySelector('.container_up')
+        .classList.remove('show_up');
+    }
+}
+
+document.querySelector('.container_up')
+.addEventListener('click',() =>{
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
